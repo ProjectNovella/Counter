24ae641317dc41fa05fe93f66d3ce9d80009ed6d
pilgwon <rlavlfrnjs12@gmail.com>
2020-11-10 13:34:45 +0900

    Initial Commit

---
+++ counter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  counter
+//
+//  Created by pilgwon on 2020/11/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
